7bcca6439ca184582480a9f14991dad0f3dd941d
probro27 <prabhavkhera@hotmail.com>
2022-05-10 16:17:14 -0400

    Interpreter done

---
+++ interpreter.hs
@@ -0,0 +1,82 @@
+import Data.Maybe (fromMaybe)
+
+data Op = Add | Sub | Mul | Div
+
+opTrans :: Op -> (Int -> Int -> Int)
+opTrans Add = (+)
+opTrans Sub = (-)
+opTrans Mul = (*)
+opTrans Div = div
+
+data Ast = Number Int | 
+            BinOp Op Ast Ast | 
+            Fun String Ast | 
+            App Ast Ast | 
+            Var String |
+            With (String, Ast) Ast |
+            Set String Ast |
+            Seq Ast Ast
+            deriving (Eq, Show)
+
+type Loc = Int
+type Env = [(String, Loc)]
+type Store = [(Loc, Int)]
+
+data Val = Numb Int | Closure String Env Loc | Void deriving (Eq, Show)
+
+
+newtype State a = S (Store -> (a, Store))
+
+app :: State a -> Store -> (a, Store)
+app (S st) str = st str
+
+instance Functor State where
+    -- fmap :: (a -> b) -> State a -> State b
+    fmap g st = S (\s -> let (x, s') = app st s in (g x, s'))
+
+instance Applicative State where
+    -- pure :: a -> State a
+    pure x = S (\s -> (x, s))
+    
+    -- (<*>) :: State (a -> b) -> State a -> State b
+    stf <*> stx = S (\s -> 
+        let (f, s0) = app stf s in 
+            let (x, s1) = app stx s0 in (f x, s1))
+
+instance Monad State where
+    -- (>>=) :: State a -> (a -> State b) -> State b
+    st >>= f = S (\s -> let (x, s0) = app st s in app (f x) s0)
+
+getVar :: Loc -> State Val
+getVar loc = S (\str -> (fromMaybe undefined (lookup loc str), str))
+
+setVar :: Loc -> Val -> State Val
+setVar loc nv = S (\str -> (Void, (loc, nv) : str))
+
+newloc :: State Loc
+newloc = S (\str -> (length str, str))
+
+interp :: Ast -> Env -> State Val
+interp (Number v) _ = return $ Numb v
+interp (Fun p b) e = return $ Closure p b e
+interp (With (var, exp) bdy) e = interp (App (Fun var bdy) exp) e
+interp (Bin op x y) e = do (Numb v) <- interp x e
+                           (Numb w) <- interp y e
+                           return $ Numb (opTrans op v w)
+interp (Seq x y) e = interp x e >> interp y e
+interp (Var x) e = return (fromMaybe undefined (lookup x e)) >>= getVar
+interp (App f x) e = do (Closure fp fb fe) <- interp f e
+                        x' <- interp x e
+                        nl <- newloc
+                        setVar nl x'
+                        interp fb $ (fp, nl) : fe
+interp (Set x y) e = do let lx = fromMaybe undefined (lookup x e)
+                        nv <- interp y e
+                        setVar lx nv
+                        return Void
+
+run :: Ast -> Val
+run ast = let (val, _) = app (interp ast []) [] in val
+
+runDebug :: Ast -> (Val, Store)
+runDebug ast = app (interp ast []) []
